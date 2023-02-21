7996820fd1dac314b22078d51165eccddb8e115d
thomas hunckler <th@phytodia.com>
2023-02-21 16:11:04 +0100

    change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def plantes
+  end
+  def analyses
+  end
+  def cosmetologie
+  end
+  def marques
+  end
+  def propos
+  end
+  def blog
+  end
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#home"
+
+  get "/laboratoire-plantes", to: "pages#plante"
+  get "/laboratoire-analyse-recherche-developpement", to: "pages#analyses"
+  get "/laboratoire-creation-cosmetique", to: "pages#cosmetologie"
+  get "/nos-marques-actifs", to: "pages#marques"
+  get "/a-propos", to: "pages#propos"
+  get "/blog", to: "pages#blog"
+  get "contact", to: "pages#contact"
 end
