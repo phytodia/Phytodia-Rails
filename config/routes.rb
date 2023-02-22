9964387c168978e50d0793c3362425104d2e4203
thomas hunckler <th@phytodia.com>
2023-02-22 08:04:14 +0100

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#home"
 
-  get "/laboratoire-plantes", to: "pages#plante"
+  get "/laboratoire-plantes", to: "pages#plantes"
   get "/laboratoire-analyse-recherche-developpement", to: "pages#analyses"
   get "/laboratoire-creation-cosmetique", to: "pages#cosmetologie"
   get "/nos-marques-actifs", to: "pages#marques"
