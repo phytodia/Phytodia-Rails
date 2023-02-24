e5a3d2803a37fc9745f6f7f6656d75ef80a06781
thomas hunckler <th@phytodia.com>
2023-02-24 20:57:20 +0100

    change

--- db/migrate/20230223082827_create_blog_posts.rb
+++ db/migrate/20230223082827_create_blog_posts.rb
@@ -2,7 +2,7 @@ class CreateBlogPosts < ActiveRecord::Migration[7.0]
   def change
     create_table :blog_posts do |t|
       t.string :titre
-      t.rich_text :content
+      t.text :body
 
       t.timestamps
     end
