70b7e38622f4b8fb2b8a63c75b9f5ccf7d7d4f2c
thomas hunckler <th@phytodia.com>
2023-02-28 16:44:18 +0100

    change css

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,11 +3,29 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="navbar"
 export default class extends Controller {
   connect() {
-    let el = document.querySelector('p');
-    el.textContent = "Hello World! + Stimulus";
+    //let el = document.querySelector('p');
+    //el.textContent = "Hello World! + Stimulus";
     console.log("trst")
+    let navHead = document.querySelectorAll(".nav-head")
+    navHead.forEach(element=>{
+      element.addEventListener("mouseover", (event)=>{
+        document.querySelector(".sub-nav").style.visibility = 'visible'
+      })
+      element.addEventListener("mouseout", (event)=>{
+        document.querySelector(".sub-nav").style.visibility = 'hidden'
+      })
+    })
+    document.querySelector(".sub-nav").addEventListener("mouseover", (event)=>{
+      //document.querySelector(".sub-nav").style.visibility = 'visible'
+    })
+    document.querySelector(".sub-nav").addEventListener("mouseout", (event)=>{
+      document.querySelector(".sub-nav").style.visibility = 'hidden'
+    })
   }
   greet(){
     console.log("test 2")
+  }
+  navDisplay(){
+    console.log("navDisplay")
   }
 }
